ac0ed9154efe29a3659552024984a497b9d4c454
Kholid <nrkholid1@gmail.com>
2024-06-20 14:13:30 +0700

    New Odoo Module

---
+++ sanbe_contacts/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name" : "PT. Sanbe Farma - Custom Contact",
+    "description" : "Pelatihan Technical Odoo V17 untuk PT. Sanbe Farma",
+    "author" : "PT. Sanbe Farma, PT. Arkana Solusi Digital",
+    "version" : "17.0.1.0.0",
+    "category" : "Others",
+    "license" : "OPL-1",
+    "depends" : [
+        "base", 
+        "contacts"
+    ],
+    "data" : [
+        "security/ir.model.access.csv",
+    ],
+    "auto_install" : False, 
+    "installable" : True,
+    "application" : True,
+    "external_dependencies" : {"python" : []}
+}
